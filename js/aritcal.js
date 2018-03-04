5f103fbb85efdc719f47a9c0aaba4a3508213704
David Zheng <nmdigboy@hotmail.com>
2018-03-05 02:02:43 +0800

    细节完善文章列表页

--- js/aritcal.js
+++ js/aritcal.js
@@ -22,6 +22,7 @@ function getArticleTypes(){
 				alert("还没有文章信息");
 			}
 			else{
+				var allTypeNum=res.data.length;
 				$.each(res.data,function(i,o){
 					$("#artical_list").append(listItemHtml);
 					$("#artical_list > li:last-child").attr('data-tid',o.id).find('a').attr('href','articlelist.html?typeid='+o.id).text(o.name);
@@ -39,13 +40,10 @@ function getArticleByTypeID(){
 	var typeID=GetQueryString('typeid');
 	if(typeID=="" && $("#artical_list > li").length > 0){
 		typeID=$("#artical_list > li:first-child").data('tid');
-	}else{
-		alert("获取文章列表出错~");
-		return;
 	}
 	
 	var artiListItemHtml='<div class="arri-block">\
-		     <img class="arti-pic" src="images/111.jpg" width="290" height="170" />\
+		     <a class="arti-pic-a" href=""><img class="arti-pic" src="images/111.jpg" width="290" height="170" /></a>\
 		     <a class="arti-title" href="#">文章标题</a> <br>\
 			 <a class="arti-auth" href="#">作者</a>\
 			 <div class="block-bottom">\两天前 </div>\
@@ -60,9 +58,10 @@ function getArticleByTypeID(){
 			}else{
 				$.each(res.data.rows,function(i,o){
 					$("#right_content").append(artiListItemHtml);
-					$("#right_content > div").find('img.arti-pic').attr('src',o.coverUrl)
-						.end().find('a.arti-title').text(o.title)
-						.end().find('a.arti-auth').text(o.userName)
+					$("#right_content > div:last-child").find('img.arti-pic').attr('src',o.coverUrl)
+						.end().find('a.arti-pic-a').attr('href','article.html?aid='+o.id)
+						.end().find('a.arti-title').attr('href','article.html?aid='+o.id).text(o.title)
+						.end().find('a.arti-auth').attr('href','usercenter.html?sid='+o.userId).text(o.userName)
 					    .end().find('div.block-bottom').text(getDateDiff(o.createTime));
 				})
 			}
